9eea984154fe932ee1760d6c95e5fcf450afae40
Kiran Kumar <kirankumar@10decoders.in>
2022-10-08 18:36:08 +0530

    Fixed App crash after few second

--- android/src/main/java/com/reactnativepedometerdetails/step/background/Restarter.kt
+++ android/src/main/java/com/reactnativepedometerdetails/step/background/Restarter.kt
@@ -11,7 +11,8 @@ class Restarter : BroadcastReceiver() {
 
         // restart the step counting service (different code to achieve this depending on Android version)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(Intent(context, StepCounterService::class.java))
+            // context.startForegroundService(Intent(context, StepCounterService::class.java))
+            context.startService(Intent(context, StepCounterService::class.java))
         } else {
             context.startService(Intent(context, StepCounterService::class.java))
         }
